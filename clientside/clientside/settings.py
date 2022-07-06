b480f2c7aa6c90580207ada04a16f0bab13cc6e6
evansbestmann <42318259+evansbestmann@users.noreply.github.com>
2022-07-06 23:58:49 +0100

    inital commit004

--- clientside/clientside/settings.py
+++ clientside/clientside/settings.py
@@ -15,8 +15,8 @@ import os
 import django_heroku 
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -130,12 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR , 'static')
+STATIC_URL = os.path.join[(BASE_DIR , 'static')]
 #STATIC_URL = '/static/'
 
 MEDIA_URL= '/test/'
 #MEDIA_ROOT= BASE_DIR/'DUMP'
-MEDIA_ROOT=os.path.join(BASE_DIR , 'DUMP')
+MEDIA_ROOT=os.path.join[(BASE_DIR , 'DUMP')]
 #STATIC_ROOT= BASE_DIR/'static_root'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
