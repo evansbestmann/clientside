568cfc6e083c7d50872000d05588939334cc8113
evansbestmann <42318259+evansbestmann@users.noreply.github.com>
2022-07-07 00:07:31 +0100

    inital commit005

--- clientside/clientside/settings.py
+++ clientside/clientside/settings.py
@@ -130,12 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = os.path.join[(BASE_DIR , 'static')]
+STATIC_URL = os.path.join(BASE_DIR , 'static')
 #STATIC_URL = '/static/'
 
 MEDIA_URL= '/test/'
 #MEDIA_ROOT= BASE_DIR/'DUMP'
-MEDIA_ROOT=os.path.join[(BASE_DIR , 'DUMP')]
+MEDIA_ROOT=os.path.join(BASE_DIR , 'DUMP')
 #STATIC_ROOT= BASE_DIR/'static_root'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
