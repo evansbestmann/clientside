88e15a00633955d7765a9841254fd677fed92c5a
evansbestmann <42318259+evansbestmann@users.noreply.github.com>
2022-07-07 08:35:34 +0100

    inital commit007

--- clientside/clientside/settings.py
+++ clientside/clientside/settings.py
@@ -142,8 +142,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-#STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"#part of heroku config compress
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"#part of heroku config compress
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 EMAIL_BACKEND = 'django_mailjet.backends.MailjetBackend'
